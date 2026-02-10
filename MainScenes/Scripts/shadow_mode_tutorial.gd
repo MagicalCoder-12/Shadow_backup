3d253fec0c4e679d0bd202a229ca3fcb56281b1a
MagicalCoder-12 <MagicalCoder-12@users.noreply.github.com>
2026-02-10 12:40:54 +0000

    refactor(shadow-tutorial): route level progression and save actions via GameManager facade

--- MainScenes/Scripts/shadow_mode_tutorial.gd
+++ MainScenes/Scripts/shadow_mode_tutorial.gd
@@ -6,22 +6,17 @@ func _on_continue_button_pressed() -> void:
 	get_parent().queue_free()
 	
 	# Properly complete the level and unlock the next level
-	var current_level = GameManager.level_manager.get_current_level()
+	var current_level = GameManager.get_current_level()
 	
 	# Mark level 5 as completed if it's not already
-	if not GameManager.level_manager.completed_levels.has(current_level):
-		GameManager.level_manager.completed_levels.append(current_level)
-		GameManager.level_star_earned.emit(current_level)
+	GameManager.mark_level_completed_if_needed(current_level)
 	
 	# Unlock next level (level 6)
 	var next_level = current_level + 1
-	if next_level > GameManager.level_manager.unlocked_levels:
-		GameManager.level_manager.unlocked_levels = next_level
-		GameManager.level_unlocked.emit(next_level)
+	GameManager.unlock_level_if_needed(next_level)
 	
 	# Save progress
-	if GameManager.save_manager.autosave_progress:
-		GameManager.save_manager.save_progress()
+	GameManager.save_progress_if_enabled()
 	
 	# Transition to the map scene to show the unlocked level
-	GameManager.change_scene(GameManager.scene_manager.MAP_SCENE)
+	GameManager.change_scene(GameManager.get_map_scene_path())
